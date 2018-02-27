bb39a68da49db1530b961ad04ea3662dc8a69e2b
Mark Page <mark.page@whitelamp.com>
2018-02-27 16:30:41 +0000

    Last big bug fix after atomising projects

--- app/config/static/Swef/SwefCMS.requires.php
+++ app/config/static/Swef/SwefCMS.requires.php
@@ -1,11 +1,11 @@
 <?php
 
 // SYSTEM CONSTANTS
-require_once SWEF_CONFIG_PATH.'/static/Swef/SwefCMS.defines.php';
+require_once SWEF_DIR_CONFIG.'/static/Swef/SwefCMS.defines.php';
 
 
 // CONFIGURABLE CONSTANTS
-require_once SWEF_CONFIG_PATH.'/user/Swef/SwefCMS.config.php';
+require_once SWEF_DIR_CONFIG.'/user/Swef/SwefCMS.config.php';
 
 
 // PLUGINS
